7c0cc8c7591a0fa94267a907fa5e9bd73b5ef049
jlb-1985 <130216366+jlb-1985@users.noreply.github.com>
2024-09-10 22:33:45 +0200

    Added  moment.js and timezones functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-const video = document.getElementById('background-video').playbackRate = 0.5; ;
+//Tarawa//
+function updateTime(){
+
+let tarawaElement = document.querySelector("#tarawa");
+let tarawaDateElement = tarawaElement.querySelector(".date");
+let tarawaTimeElement = tarawaElement.querySelector(".time");
+let tarawaTime= moment().tz("Pacific/Tarawa");
+
+tarawaDateElement.innerHTML = tarawaTime.format('MMMM Do YYYY');
+tarawaTimeElement.innerHTML = tarawaTime.format('h:mm:ss [<small>]A[</small>]');
+
+//Pago pago//
+
+let pagoPagoElement = document.querySelector("#pago-pago");
+let pagoPagoDateElement = pagoPagoElement.querySelector(".date");
+let pagoPagoTimeElement = pagoPagoElement.querySelector(".time");
+let pagoPagoTime= moment().tz("Pacific/Pago_Pago");
+
+pagoPagoDateElement.innerHTML = pagoPagoTime.format('MMMM Do YYYY');
+pagoPagoTimeElement.innerHTML = pagoPagoTime.format('h:mm:ss [<small>]A[</small>]');
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+
+
+
+
+
+const video = document.getElementById('background-video').playbackRate = 0.4; ;
 
 video.addEventListener('timeupdate', () => {
     if (video.currentTime >= video.duration - 0.5) {
@@ -6,4 +36,3 @@ video.addEventListener('timeupdate', () => {
         video.play();
     }
 });
-
