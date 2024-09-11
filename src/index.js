23bd077cdf80895fa91a8e197ce12f3f47506f70
jlb-1985 <130216366+jlb-1985@users.noreply.github.com>
2024-09-11 02:03:33 +0200

    Added the functionality for select

--- src/index.js
+++ src/index.js
@@ -1,38 +1,49 @@
-//Tarawa//
-function updateTime(){
+function updateTime() {
+    let tarawaElement = document.querySelector("#tarawa");
+    let tarawaDateElement = tarawaElement.querySelector(".date");
+    let tarawaTimeElement = tarawaElement.querySelector(".time");
+    let tarawaTime = moment().tz("Pacific/Tarawa");
+  
+    tarawaDateElement.innerHTML = tarawaTime.format('MMMM Do YYYY');
+    tarawaTimeElement.innerHTML = tarawaTime.format('h:mm:ss [<small>]A[</small>]');
+  
+    let pagoPagoElement = document.querySelector("#pago-pago");
+    let pagoPagoDateElement = pagoPagoElement.querySelector(".date");
+    let pagoPagoTimeElement = pagoPagoElement.querySelector(".time");
+    let pagoPagoTime = moment().tz("Pacific/Pago_Pago");
+  
+    pagoPagoDateElement.innerHTML = pagoPagoTime.format('MMMM Do YYYY');
+    pagoPagoTimeElement.innerHTML = pagoPagoTime.format('h:mm:ss [<small>]A[</small>]');
+  }
+  
+  function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+      </div>
+    `;
+  }
+  
+  updateTime();
+  setInterval(updateTime, 1000);
+  
+  let citiesSelectElement = document.querySelector("#city");
+  citiesSelectElement.addEventListener("change", updateCity);
 
-let tarawaElement = document.querySelector("#tarawa");
-let tarawaDateElement = tarawaElement.querySelector(".date");
-let tarawaTimeElement = tarawaElement.querySelector(".time");
-let tarawaTime= moment().tz("Pacific/Tarawa");
+  const video = document.getElementById('background-video');
+  video.playbackRate = 0.4;
 
-tarawaDateElement.innerHTML = tarawaTime.format('MMMM Do YYYY');
-tarawaTimeElement.innerHTML = tarawaTime.format('h:mm:ss [<small>]A[</small>]');
-
-//Pago pago//
-
-let pagoPagoElement = document.querySelector("#pago-pago");
-let pagoPagoDateElement = pagoPagoElement.querySelector(".date");
-let pagoPagoTimeElement = pagoPagoElement.querySelector(".time");
-let pagoPagoTime= moment().tz("Pacific/Pago_Pago");
-
-pagoPagoDateElement.innerHTML = pagoPagoTime.format('MMMM Do YYYY');
-pagoPagoTimeElement.innerHTML = pagoPagoTime.format('h:mm:ss [<small>]A[</small>]');
-}
-
-updateTime();
-setInterval(updateTime, 1000);
-
-
-
-
-
-
-const video = document.getElementById('background-video').playbackRate = 0.4; ;
-
-video.addEventListener('timeupdate', () => {
+  video.addEventListener('timeupdate', () => {
     if (video.currentTime >= video.duration - 0.5) {
-        video.currentTime = 0;
-        video.play();
+      video.currentTime = 0;
+      video.play();
     }
-});
+  });
